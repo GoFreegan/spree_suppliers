dfb784acf78b7ed4817baac6ffab4ceee35424e2
comeonandroid <comeonandroid@gmail.com>
2016-10-11 14:23:04 +0300

    change paths

--- lib/generators/spree_suppliers/install/install_generator.rb
+++ lib/generators/spree_suppliers/install/install_generator.rb
@@ -5,13 +5,13 @@ module SpreeSuppliers
       include Rails::Generators::Migration
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_suppliers\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_suppliers\n"
+        append_file "app/assets/javascripts/frontend/all.js", "//= require store/spree_suppliers\n"
+        append_file "app/assets/javascripts/backend/all.js", "//= require admin/spree_suppliers\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_suppliers\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_suppliers\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/frontend/all.css", " *= require store/spree_suppliers\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/backend/all.css", " *= require admin/spree_suppliers\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
